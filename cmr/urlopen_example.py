908658dbf0d1aef95989181cddfb6223421a7eba
Jean Flower <jeanflower@rocketmail.com>
2017-07-15 18:19:07 +0100

    Add *_example.py scripts

---
+++ cmr/urlopen_example.py
@@ -0,0 +1,15 @@
+#use urlopen to obtain html from a url
+from urllib.request import urlopen
+
+#choose a web site we want to visit
+url="http://www.google.co.uk"
+    
+#go to the given url and obtain the html 
+html = urlopen(url)
+
+#show what we got - it's an HTTPResponse object
+print(html)
+
+#call read() on the HTTPResponse object
+#(get a lot back from a simple call to google!)
+print(html.read())
