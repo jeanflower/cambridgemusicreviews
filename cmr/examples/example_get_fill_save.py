6f9cfbe4d5c8ce54ae80745844a154d6b5fa4083
Jean Flower <jeanflower@rocketmail.com>
2017-07-22 18:01:22 +0100

    testing interaction

---
+++ cmr/examples/example_get_fill_save.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+from cmr.cmr_get_articles_from_webpage import get_all_cmr_data
+from cmr.cmr_interactive import fill_in_missing_data, \
+                                get_missing_index_text_interactive, \
+                                get_missing_category_interactive
+from cmr.cmr_create_index_html import save_index_html
+
+
+articles = get_all_cmr_data()
+
+fill_in_missing_data(articles,
+                     get_missing_index_text_interactive, 
+                     get_missing_category_interactive)
+
+
+save_index_html(articles, "test.html")
+
+
+
