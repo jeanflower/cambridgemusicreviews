776e378ea7a4a9225a2e357d8ab62363c39d60b8
Jean Flower <jeanflower@rocketmail.com>
2017-07-23 08:52:08 +0100

    fix non-test interaction for missing index data

--- make_index_html.py
+++ make_index_html.py
@@ -4,7 +4,11 @@ from cmr.cmr_get_articles_from_webpage import get_all_cmr_data
 
 from cmr.cmr_interactive import fill_in_missing_data, \
                                 get_missing_index_text_interactive, \
-                                get_missing_category_interactive
+                                get_missing_category_interactive, \
+                                confirm_is_single_interactive, \
+                                confirm_is_album_interactive, \
+                                confirm_is_live_interactive
+                                
 from cmr.cmr_create_index_html import save_index_html
 
 
@@ -12,7 +16,10 @@ articles = get_all_cmr_data()
 
 fill_in_missing_data(articles,
                      get_missing_index_text_interactive, 
-                     get_missing_category_interactive)
+                     get_missing_category_interactive,
+                     confirm_is_single_interactive,
+                     confirm_is_album_interactive,
+                     confirm_is_live_interactive)
 
 sort_articles(articles)
 
