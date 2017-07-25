f33ce8372fea3315659eb4debf5bca143ced148b
Jean Flower <jeanflower@rocketmail.com>
2017-07-25 08:21:40 +0100

    Add top-level function to capture existing pages

---
+++ capture_cmr_pages.py
@@ -0,0 +1,5 @@
+from cmr.cmr_generate_test_data import generate_test_data
+
+# this will generate a set of files called page_text_1.html etc
+# after loading the data from the CMR wordpress site
+generate_test_data()
--- cmr/cmr_generate_test_data.py
+++ cmr/cmr_generate_test_data.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 
-from cmr_utilities import get_cmr_url, save_html
+from cmr.cmr_utilities import get_cmr_url, save_html
 
 # today, after the 21st page, we get an error
-max_page_number = 40
+max_page_number = 200
 
-# this will generate a set of files called page_text_1.html etc
-# after loading the data from the CMR wordpress site
-for page_number in range(1, max_page_number):
-    url = get_cmr_url(page_number)
-    save_html(url, "../tests/page_text_"+str(page_number)+".html")
+def generate_test_data():
+    # this will generate a set of files called page_text_1.html etc
+    # after loading the data from the CMR wordpress site
+    for page_number in range(1, max_page_number):
+        url = get_cmr_url(page_number)
+        if not save_html(url, "page_text_"+str(page_number)+".html"):
+            break
 
