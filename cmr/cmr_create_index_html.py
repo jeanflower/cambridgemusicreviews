3bbfab9f0a821b0836f4c544662a809c9b3086eb
Jean Flower <jeanflower@rocketmail.com>
2017-07-26 23:31:12 +0100

    Remove temp example code

--- cmr/cmr_create_index_html.py
+++ cmr/cmr_create_index_html.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from cmr.cmr_utilities import CMR_Article, CMR_Index_Categories
+from cmr.cmr_utilities import CMR_Index_Categories
 
 def get_html_link(article):
     html = "<a href=\""
@@ -58,23 +58,4 @@ def save_index_html(articles, filename):
     f = open(filename, 'w')
     f.write(html)
     f.close()
-
-#Set up an example of a couple of articles we might want to convert into
-#an index
-sample_article_0 = CMR_Article()
-sample_article_0.title = "ABC, Parker’s Piece, 7 July 2017"
-sample_article_0.url = "http://example_url_0.com"
-sample_article_0.index_text = "ABC, 7th July 2017"
-sample_article_0.category = CMR_Index_Categories.album
-
-sample_article_1 = CMR_Article()
-sample_article_1.title = "Lee Hull, Corner House, Cambridge, 4 June 2017"
-sample_article_1.url = "http://example_url_1.com"
-sample_article_1.index_text = "Lee Hull, 4th June 2017"
-sample_article_1.category = CMR_Index_Categories.live
-
-articles = [sample_article_0, sample_article_1]
-
-#print(get_index_html(articles))
-#save_index_html(articles, "test.html")
 
