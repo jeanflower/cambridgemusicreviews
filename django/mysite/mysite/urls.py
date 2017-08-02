3f532a5761eefef8ce0f1f0e9fc9cf5bcf63ba08
Jean Flower <jeanflower@rocketmail.com>
2017-08-02 22:30:10 +0100

    Moved web app from web to django, switched to official django tutorial

--- django/mysite/mysite/urls.py
+++ django/mysite/mysite/urls.py
@@ -1,4 +1,4 @@
-"""HelloWorld URL Configuration
+"""mysite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
@@ -13,16 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
-
-from HelloWorldApp.views import generate_text_for_web_display
-
-#from django.contrib import admin
-#admin.autodiscover()
+from django.conf.urls import include,url
+from django.contrib import admin
 
 urlpatterns = [
-    #url(r'^admin/', include(admin.site.urls)),
-    # ... your url patterns
-    url(r'HelloWorldApp/$', generate_text_for_web_display)
+    url(r'^polls/', include('polls.urls')),
+    url(r'^admin/', admin.site.urls),
 ]
-
