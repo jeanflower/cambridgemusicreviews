bdfc7e5277c5c1a340d4653b1b2a5b0dbf09ccb2
Jean Flower <jeanflower@rocketmail.com>
2017-08-03 21:13:31 +0100

    Have django polls view show cmr index

--- django/mysite/polls/views.py
+++ django/mysite/polls/views.py
@@ -1,6 +1,22 @@
 from django.http import HttpResponse
+import sys
+sys.path.append('../..')
 
+from importlib import import_module
+import_module("cmr.cmr_create_index_html")
+
+from cmr.cmr_get_articles_from_webpage import get_all_cmr_data
+from cmr.cmr_interactive import fill_in_missing_data_interactive
+from cmr.cmr_create_index_html import get_index_doc_html
+from cmr.cmr_utilities import sort_articles
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    articles = get_all_cmr_data()
+    fill_in_missing_data_interactive(articles)
+    sort_articles(articles)
+    html = get_index_doc_html(articles)
+    return HttpResponse(html)
+
+#def index(request):
+#    return HttpResponse("Hello, world. You're at the polls index.")
 
