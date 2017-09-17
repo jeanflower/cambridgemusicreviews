043343a301fcb60a3bb2c321d1d30cc65a455756
Jean Flower <jeanflower@rocketmail.com>
2017-09-17 11:24:01 +0100

    Add comment

--- django/cmr_site/indexer/models.py
+++ django/cmr_site/indexer/models.py
@@ -28,7 +28,7 @@ class Article(models.Model):
     title = models.CharField( max_length = 200, default = "" )      # e.g. "ABC, Parker’s Piece, Cambridge, 7 July\xa02017"
     url = models.CharField( max_length = 400, default = "" )        # e.g. "https://cambridgemusicreviews.net/2017/07/09/abc-parkers-piece-cambridge-7-july-2017/"
     index_text = models.CharField( max_length = 200, default = "" ) # e.g. "ABC"
-    category = models.IntegerField( default = 0 )     # e.g. CMR_Index_Categories.live
+    category = models.IntegerField( default = 0 )     # e.g. CMR_Index_Categories.live enum value
     index_status = models.IntegerField( default = 0 )
 
     def __str__(self):
