6ee756647634db979ba46c0d9af65c33532d45dc
Jean Flower <jeanflower@rocketmail.com>
2017-10-01 09:14:15 +0100

    add edit to web index view

--- django/cmr_site/indexer/models.py
+++ django/cmr_site/indexer/models.py
@@ -4,10 +4,12 @@ import sys
 sys.path.append('../..')
 
 
+max_index_text_length = 100
+
 class Article(models.Model):
     title = models.CharField( max_length = 200, default = "" )      # e.g. "ABC, Parker’s Piece, Cambridge, 7 July\xa02017"
     url = models.CharField( max_length = 400, default = "" )        # e.g. "https://cambridgemusicreviews.net/2017/07/09/abc-parkers-piece-cambridge-7-july-2017/"
-    index_text = models.CharField( max_length = 200, default = "" ) # e.g. "ABC"
+    index_text = models.CharField( max_length = max_index_text_length, default = "" ) # e.g. "ABC"
     category = models.IntegerField( default = 0 )     # e.g. CMR_Index_Categories.live enum value
     index_status = models.IntegerField( default = 0 )
 
